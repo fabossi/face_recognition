1c413007b4e73119660f44a31152fe1915712663
Felipe Fabossi <felipe.augusto.fabossi@ibm.com>
2020-06-24 14:13:20 -0300

    [FABOSSI] 100% Complete

--- findfaces.py
+++ findfaces.py
@@ -1,6 +1,6 @@
 import face_recognition
 
-image = face_recognition.load_image_file("./img/felipe.jpg")
+image = face_recognition.load_image_file("./img/groups/team2.jpg")
 face_locations = face_recognition.face_locations(image)
 
 # Array of coordenates
